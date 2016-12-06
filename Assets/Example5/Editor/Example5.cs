6ffc8019d6bd46ea78be94da3e37d2617b5ac030
Jules Robichaud-Gagnon <jules.robichaud@solutionstlm.com>
2016-12-05 21:25:14 -0500

    Adds confitional attribute example

---
+++ Assets/Example5/Editor/Example5.cs
@@ -0,0 +1,32 @@
+﻿#define DEFINED
+
+using NUnit.Framework;
+using System.Diagnostics;
+using System;
+
+public class Example5_Conditional {
+
+	[Conditional("NOT_DEFINED")]
+	void NotDefinedMethod(){
+		throw new Exception();
+	}
+
+	[Conditional("DEFINED")]
+	void DefinedMethod(){
+		throw new Exception();
+	}
+
+	[Test]
+	public void NotDefinedIsNotCalled() {
+		Assert.DoesNotThrow( ()=>{
+			NotDefinedMethod();
+		});
+	}
+
+	[Test]
+	public void DefinedIsCalled() {
+		Assert.Throws<Exception>( ()=>{
+			DefinedMethod();
+		});
+	}
+}
